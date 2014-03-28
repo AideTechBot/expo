5353c38ce356442863d3fe8f3aec07f2c49b3154
Manuel Dionne <manimo41@hotmail.com>
2014-03-28 10:17:11 -0300

    Removed unused or outdated header files

--- pi/gps c/gps.c
+++ pi/gps c/gps.c
@@ -10,13 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
-#include <netdb.h>
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-
-#include <arpa/inet.h>
 
 #define FNAME "send.wav"
 #define BAUD 60
